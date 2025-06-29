6e09ba812bf81d96d4e90f4b4cc89d0acef64046
Yashwanthv-28 <yash7777700@gmail.com>
2025-06-29 23:26:32 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -45,7 +45,9 @@ const Home = () => {
           A passionate Full Stack Developer blending design thinking with problem-solving. I build intuitive web solutions that don't just work—but *feel* right.
 
         </p>
-        <a href="/Personal-Portfolio/Resume.pdf" download className="glass-resume-button">
+        <a href={`${process.env.PUBLIC_URL}/Resume.pdf`}
+         download 
+         className="glass-resume-button">
           <FaDownload className="download-icon" />
           Resume
         </a>
